1b7d111b374557a845420076fddc597561f59f5e
bryandmoreira <bryan.d.moreira@aluno.senai.br>
2025-11-13 16:22:30 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,53 +6,58 @@ function App() {
   const [n2, setN2] = useState()
   const [respSoma, setRespSoma] = useState()
   const [respSub, setRespSub] = useState()
-  const [respMulti, setRespMulti] = useState()
+  const [respMult, setRespMult] = useState()
   const [respDiv, setRespDiv] = useState()
+
+
 
   const [error, setError] = useState('')
 
-  useEffect(() =>{
-    if(n1 !== undefined && n2 !== undefined && !isNaN(n1) && !isNaN(n2)){
-    setRespSoma(parseFloat(n1) + parseFloat(n2))
-    setRespSub(parseFloat(n1) - parseFloat(n2))
-    setRespMulti(parseFloat(n1) * parseFloat(n2))
-    setRespDiv(parseFloat(n1) / parseFloat(n2))
-
-    setError('')
+  useEffect(() => {
+    if (n1 !== undefined && n2 !== undefined && !isNaN(n1) && !isNaN(n2)) {
+      setRespSoma(parseFloat(n1) + parseFloat(n2))
+      setRespSub(parseFloat(n1) - parseFloat(n2))
+      setRespMult(parseFloat(n1) * parseFloat(n2))
+      setRespDiv(parseFloat(n1) / parseFloat(n2))
+        setError('Digite valores númericos para calcular')
     }else{
       setError('Digite valores númericos para calcular')
     }
+
   }, [n1, n2])
   return (
-    <>
-     <h1 className={styles.title}>Calculadora</h1>
-     <div className={styles.wrap}>
-      <input type="number" onChange={(e) => setN1(e.target.value)} value={n1} placeholder='Insira um número'/>
-      <input type="number" onChange={(e) => setN2(e.target.value)} value={n2} placeholder='Insira outro número'/>
-     
-     </div>
-     
-     <div className={styles.wrapResps}>
-      <h2>Resultado da soma</h2>
-      <h3>{respSoma}</h3>
+    <div className={styles.wrapAll}>
+      <h1 className={styles.title}>Calculadora</h1>
+      <p>{error}</p>
+      <div className={styles.wrap}>
+        <input type="number" onChange={(e) => setN1(e.target.value)} value={n1} placeholder='Insira um número' />
+        <input type="number" onChange={(e) => setN2(e.target.value)} value={n2} placeholder='Insira outro número' />
+      </div>
 
-     </div>
-    <div className={styles.wrapResps}>
-      <h2>Resultado da subtração</h2>
-      <h3>{respSub}</h3>
+      <div className={styles.wrapResps}>
+        <h2>Resultado da soma</h2>
+        <h3>{respSoma}</h3>
 
+      </div>
+
+        <div className={styles.wrapResps}>
+        <h2>Resultado da subtração</h2>
+        <h3>{respSub}</h3>
+
+      </div>
+
+        <div className={styles.wrapResps}>
+        <h2>Resultado da multiplicação</h2>
+        <h3>{respMult}</h3>
+
+      </div>
+
+        <div className={styles.wrapResps}>
+        <h2>Resultado da divisão</h2>
+        <h3>{respDiv}</h3>
+
+      </div>
     </div>
-    <div className={styles.wrapResps}>
-      <h2>Resultado da multiplicação</h2>
-      <h3>{respMulti}</h3>
-
-    </div>
-    <div className={styles.wrapResps}>
-      <h2>Resultado da divisão</h2>
-      <h3>{respDiv}</h3>
-
-    </div>
-    </>
   )
 }
 
